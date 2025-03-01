90bb3244ab030c0129001cb5db9c36e0c0e33c64
SJBaek <2021E7373@gs.anyang.ac.kr>
2025-03-01 23:54:59 +0900

    2025.03.01

--- Dynamic Programming/BackJoon_1463_1로 만들기.py
+++ Dynamic Programming/BackJoon_1463_1로 만들기.py
@@ -1,23 +1,15 @@
-while(True):
-    count = 0
-    x = int(input("x 입력 : "))
+n = int(input())
+
+d = [0] * (n + 1)
+
+for i in range(2, n+1):
     
-    # test 종료
-    if x == -1:
-        break
-    while(True):
-        if x == 1:
-            break
-        elif x % 3 == 0:
-            x //= 3
-            print("x를 3으로 나누기 x = ", x)
-            count += 1
-        elif x % 2 == 0:
-            x //= 2
-            print("x를 2으로 나누기 x = ", x)
-            count += 1
-        else:
-            x -= 1
-            print("x - 1, x = ", x)
-            count += 1
-    print(count)
+    d[i] = d[i-1] + 1
+    
+    if i % 2 == 0:
+        d[i] = min(d[i], d[i // 2] + 1)
+    
+    if i % 3 == 0:
+        d[i] = min(d[i], d[i // 3] + 1)
+
+print(d[n])
